7c022662738b2addb0d8ceea742cd702f10dcfd0
mohamed <ragabmohame6@gmail.com>
2020-05-10 06:15:19 +0200

    create dispatchToProps

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -2,8 +2,11 @@ import React from 'react';
 import {Switch, Route, withRouter, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import AuthForm from '../components/AuthForm';
+import {authUser} from '../store/actions/auth';
+import {removeError} from "../store/actions/errors";
 
 const Main = () => {
+    const {authUser, errors, removeError} = props;
     return (
         <section>
             <Switch>
@@ -42,7 +45,8 @@ const Main = () => {
 function mapStateToProps(state) {
     return {
         currentUser: state.currentUser,
+        errors: state.errors
     }
 }
 
-export default withRouter(connect(mapStateToProps, null)(Main));
+export default withRouter(connect(mapStateToProps, {authUser, removeError})(Main));
