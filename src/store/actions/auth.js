ed8f1cc8e0c745087a6a3cde93f80dfd68ee2aa1
mohamed <ragabmohame6@gmail.com>
2020-05-10 16:26:46 +0200

    Revert "attatch token for existing apiCalls"
    This reverts commit c45d21eda594443327115daf1c0c172d0d3d9045.

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,7 +1,6 @@
 import {apiCall} from '../../services/api';
 import {SET_CURRENT_USER} from '../actionTypes';
 import {addError, removeError} from './errors';
-import {setTokenHeader} from "../../../../../warbler/client/src/services/api";
 
 // action creator for user
 export function setCurrentUser(user) {
@@ -11,17 +10,12 @@ export function setCurrentUser(user) {
     };
 }
 
-export function setAuthorizationToken(token) {
-    setTokenHeader(token);
-}
-
 export function authUser(type, userData) {
     return dispatch => {
         return new Promise((resolve, reject) => {
             return apiCall("post", `/api/auth/${type}`, userData)
                 .then(({token, ...user}) => {
                     localStorage.setItem("jwtToken", token);
-                    setAuthorizationToken(token)
                     dispatch(setCurrentUser(user));
                     dispatch(removeError());
                     resolve();
@@ -37,7 +31,6 @@ export function authUser(type, userData) {
 export function logout() {
     return dispatch => {
         localStorage.clear();
-        setAuthorizationToken(false);
         dispatch(setCurrentUser({}));
     }
 }
