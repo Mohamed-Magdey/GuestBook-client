74ceb4040bad5a249ee236adb295c8a0f94dc5af
mohamed <ragabmohame6@gmail.com>
2020-05-10 04:19:26 +0200

    config devTools of redux

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,11 @@ import {applyMiddleware, compose, createStore} from 'redux';
 import rootReducer from './reducers';
 import thunk from 'redux-thunk';
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export function configureStore() {
     return createStore(
         rootReducer,
-        compose(applyMiddleware(thunk))
+        composeEnhancer(applyMiddleware(thunk))
     );
 }
