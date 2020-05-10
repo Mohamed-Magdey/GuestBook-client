0b8515a470530cec67d487e9ff968b54811b347a
mohamed <ragabmohame6@gmail.com>
2020-05-10 04:22:58 +0200

    connect react with redux

--- src/containers/GuestBook.js
+++ src/containers/GuestBook.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import {Provider} from 'react-redux';
+import {configureStore} from '../store'
+
+const store = configureStore();
 
 const GuestBook = () => (
-    <div>
-
-    </div>
+    <Provider store={store}>
+        <div/>
+    </Provider>
 );
 
 export default GuestBook;
