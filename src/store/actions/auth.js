dd9d54b46c5c137b7ad65e7ecc0f008eb297476c
mohamed <ragabmohame6@gmail.com>
2020-05-10 16:30:25 +0200

    attach with correct import

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,6 +1,7 @@
 import {apiCall} from '../../services/api';
 import {SET_CURRENT_USER} from '../actionTypes';
 import {addError, removeError} from './errors';
+import {setTokenHeader} from "../../services/api";
 
 // action creator for user
 export function setCurrentUser(user) {
@@ -10,12 +11,17 @@ export function setCurrentUser(user) {
     };
 }
 
+export function setAuthorizationToken(token) {
+    setTokenHeader(token);
+}
+
 export function authUser(type, userData) {
     return dispatch => {
         return new Promise((resolve, reject) => {
             return apiCall("post", `/api/auth/${type}`, userData)
                 .then(({token, ...user}) => {
                     localStorage.setItem("jwtToken", token);
+                    setAuthorizationToken(token)
                     dispatch(setCurrentUser(user));
                     dispatch(removeError());
                     resolve();
@@ -31,6 +37,7 @@ export function authUser(type, userData) {
 export function logout() {
     return dispatch => {
         localStorage.clear();
+        setAuthorizationToken(false);
         dispatch(setCurrentUser({}));
     }
 }
