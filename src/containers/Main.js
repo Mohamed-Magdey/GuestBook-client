4d0760e0adfc97aee3c08a3e9ea546770bff6617
mohamed <ragabmohame6@gmail.com>
2020-05-10 05:22:55 +0200

    add routes for AuthForm in Main

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import {Switch, Route, withRouter, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
+import AuthForm from '../components/AuthForm';
 
 const Main = () => {
     return (
         <section>
             <Switch>
-                <Redirect exact from="/" to="signin" />
+                <Redirect exact from="/" to="signup" />
+                <Route
+                    exact
+                    path="/signin"
+                    render={props => {
+                        return (
+                            <AuthForm
+                                buttonText="Log in"
+                                heading="Welcome Back."
+                                {...props} />
+                        );
+                    }}
+                />
+                <Route
+                    exact
+                    path="/signup"
+                    render={props => {
+                        return (
+                            <AuthForm
+                                signUp
+                                buttonText="Sign up!"
+                                heading="Join To GuestBook."
+                                {...props}
+                            />
+                        );
+                    }}
+                />
             </Switch>
         </section>
     )
